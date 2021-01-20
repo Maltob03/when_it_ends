f07cad3d754153aa9f00561bcf03b0c73e732682
Maltob03 <46380730+Maltob03@users.noreply.github.com>
2021-01-20 12:14:40 +0100

    update data

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,7 +74,7 @@ var config = {
 
 
     // The data/time we want to countdown to
-    var countDownDate = new Date("Jun 3, 2024 10:00:00").getTime();
+    var countDownDate = new Date("Sep 24, 2025 10:00:00").getTime();
 
     // Run myfunc every second
     var myfunc = setInterval(function() {
